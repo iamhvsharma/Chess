744fb1dd12e075c6689f480b0542df2d8902ace2
iamhvsharma <itshvsharma@gmail.com>
2024-05-05 01:46:14 +0530

    Update

--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -1,2 +1,17 @@
 // There are many libraries to use web sockets in node js 
 // In this project we will use => npm ws 
+
+
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
